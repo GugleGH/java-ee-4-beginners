8b35168d4d9ee3c36399d04fbefedcedec718964
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-12 13:48:39 +0300

    Vargars demo.

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -10,26 +10,17 @@ package com.acme.edu;
  * @author eugene
  */
 public class Main {
-    public strictfp static void main(final String[] args) {
-        Colorr color = Colorr.R;
-        color.name();
-
-        Colorr.values();
+    public strictfp static void main(final String... args) {
+        m("", "");
     }
-}
 
-enum Colorr {
-    R, G, B
-}
+    static void m(String... args) { //vararg
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
 
-final class Color {
-    public static final Color RED = new Color(1);
-    public static final Color GREEN = new Color(2);
-    public static final Color BLUE = new Color(3);
+    static void m2(String s, String... aaa) {
 
-    private int ordinal;
-
-    private Color(int ordinal) {
-        this.ordinal = ordinal;
     }
 }
