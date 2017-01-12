4c00e674d8377f4ebc4f5fca3bd174342a8b1176
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-12 13:39:51 +0300

    Enums Demo.

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -5,41 +5,31 @@
  */
 package com.acme.edu;
 
-import java.io.File;
-import java.util.List;
-
 /**
  *
  * @author eugene
  */
 public class Main {
-    public strictfp static void main(final String[] args) { //IEEE xxx : +,-,*,/
-        int[] array = new int[4];
-        System.out.println(array.length);
-        
-        int[] arr = {1,2, 3, 5, 6};
-        m( new int[] {} );
-        
-        int[][] matrix = new int[2][];
-        System.out.println(matrix[0]);
-        matrix[0] = new int[10];
-        matrix[1] = new int[20];
-        matrix[0] = new int[] {1,2};
-        
-        Object[] arra = new Object[9];
-        arra[0] = 5; //
-        Integer iii = 6; // = new Integeer(6);
-        int ii = iii; //iii.intValue()
-        
-        Integer i1 = 1_00, i2 = 1_00;
-        Integer i3 = i1 + i2;
-        System.out.println(i3);
-        
-        System.out.println(i1 == i2);
-        
+    public strictfp static void main(final String[] args) {
+        Colorr color = Colorr.R;
+        color.name();
+
+        Colorr.values();
     }
-    
-    static void m(int[] arg) {
-        
+}
+
+enum Colorr {
+    R, G, B
+}
+
+final class Color {
+    public static final Color RED = new Color(1);
+    public static final Color GREEN = new Color(2);
+    public static final Color BLUE = new Color(3);
+
+    private int ordinal;
+
+    private Color(int ordinal) {
+        this.ordinal = ordinal;
     }
 }
