ef77b62c42c2f342f12747ca132c187c901e228c
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-12 14:29:35 +0300

    Polymorphic OOP design with abstract classes

--- src/main/java/oopdemo/Logger.java
+++ src/main/java/oopdemo/Logger.java
@@ -1,29 +1,43 @@
 package oopdemo;
 
 public class Logger {
-    private FileSaver saver = new FileSaver("////");
-    private LevelFilter filter = new LevelFilter();
+    private Saver saver = new FileSaver("////"); //Is-A
+    private Filter filter = new LevelFilter();
 
-    public void log(String message, int level) {
+    public void log(String message, int level) { //1M
        if (filter.filter(level)) {
            saver.save(message);
        }
    }
 }
 
-class FileSaver {
+class Saver {
+    public void save(String message) {
+        //.....
+    }
+}
+
+class FileSaver extends Saver {
     private String path;
 
     FileSaver(String path) {
         this.path = path;
     }
 
+    @Override
     public void save(String message) {
-
+        //......IO
+        super.save(message);
+        //......
     }
 }
 
-class LevelFilter {
+abstract class Filter {
+    public abstract boolean filter(int level);
+}
+
+class LevelFilter extends Filter {
+    @Override
     public boolean filter(int level) {
         return false;
     }
