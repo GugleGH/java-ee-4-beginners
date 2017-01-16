376eee176377b9826b6a390829fdd7eee0488888
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-16 18:21:36 +0300

    Add collection API demo.

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -6,8 +6,7 @@
 package com.acme.edu;
 
 
-import java.io.*;
-import java.sql.SQLException;
+import java.util.*;
 
 /**
  *
@@ -15,41 +14,8 @@ import java.sql.SQLException;
  */
 public class Main {
     public static void main(String[] args) {
-        try {
-            readFile();
-            //.....
-            //......
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    static void readFile() throws MyBLException {
-        /*
-        if (true) try {
-            throw new IOException("kdjfhkdfjghdkf!!!!!!");
-        } catch (IOException e) {
-            ///......
-            if(1==1) throw new RuntimeException("rrr");
-            e.printStackTrace();
-            throw new MyBLException("fdkgjhdfgkjdhfgkjdhg", e);
-        } finally {
-            throw new RuntimeException("d");
-        }
-        */
-
-        try (My my = new My()) {
-            throw new IOException("1");
-        } catch (IOException e) {
-            throw new RuntimeException("2");
-        }
-        //....
-    }
-}
-
-class My implements Closeable {
-    @Override
-    public void close() throws IOException {
-        throw new IOException("qqqqq");
+        Collections.unmodifiableList(new ArrayList<>()).add("");
+        Arrays.<String>asList("");
+        Collections.emptyList();
     }
 }
